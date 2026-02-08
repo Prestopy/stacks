3d48277242874ed3bf39bbfdb1f37b3aa0209ff4
Kingston Tsai <65468855+Prestopy@users.noreply.github.com>
2026-02-08 11:13:46 +0800

    Refactor UpcomingLayout to improve task and deadline handling, add extra block for future items

--- app/Components/UpcomingLayout.tsx
+++ app/Components/UpcomingLayout.tsx
@@ -1,14 +1,18 @@
-import {
-	TaskItemBlock,
-	TaskItemModifications,
-} from "@/app/util/types/types";
+import { TaskItemBlock, TaskItemModifications } from "@/app/util/types/types";
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import { Actions } from "@/app/util/types/typeUtilities";
 import TaskItemLayout from "@/app/Components/TaskItemLayout";
 import { useMemo } from "react";
-import { isSameDay, isToday, toDate, toDateOrUndefined, today, toExactDate } from "@/app/util/dateUtilities";
+import {
+	isSameDay,
+	isToday,
+	toDate,
+	toDateOrUndefined,
+	today,
+	toExactDate,
+} from "@/app/util/dateUtilities";
 import { IconPlus } from "@tabler/icons-react";
-import { format } from "date-fns";
+import { addDays, differenceInCalendarDays, format } from "date-fns";
 import RichIcon from "@/app/Components/RichIcon";
 import Constants from "@/app/util/constants";
 
@@ -31,6 +35,7 @@ interface DateBlock {
 	value: Date;
 	children: (TaskItemBlock | DeadlineBlock)[];
 }
+
 interface DeadlineBlock {
 	kind: "deadline";
 	value: {
@@ -40,6 +45,7 @@ interface DeadlineBlock {
 		date: Date;
 	};
 }
+
 export default function UpcomingLayout({
 	taskItems,
 	allProjects,
@@ -50,11 +56,8 @@ export default function UpcomingLayout({
 	const blocks = useMemo(() => {
 		const folderBlocks: DateBlock[] = [];
 
-		// The next 12 months TODO
-		// The next 14 days including today should be dates
-		// Everything else be months
 
-		for (let i = 0; i < 14; i++) {
+		for (let i = 0; i < 7; i++) {
 			const date = new Date();
 			date.setDate(date.getDate() + i);
 			folderBlocks.push({
@@ -66,41 +69,87 @@ export default function UpcomingLayout({
 
 		for (const folderBlock of folderBlocks) {
 			folderBlock.children = taskItems
-				.filter(i => i.startDate !== undefined || i.deadline !== undefined)
-				.map(
-					(item) => {
-						const itemDate = toDateOrUndefined(item.startDate);
-						const itemDeadline = toDateOrUndefined(item.deadline);
-						if (itemDate !== undefined && !item.isCompleted) {
-							if (isSameDay(itemDate, folderBlock.value)) {
-								return {
-									kind: "taskItem",
-									value: item,
-								} as TaskItemBlock
-							}
+				.filter((i) => i.startDate !== undefined || i.deadline !== undefined)
+				.map((item) => {
+					const itemDate = toDateOrUndefined(item.startDate);
+					const itemDeadline = toDateOrUndefined(item.deadline);
+					if (itemDate !== undefined && !item.isCompleted) {
+						// FIXME: If the start date == deadline, only start date will show
+						if (isSameDay(itemDate, folderBlock.value)) {
+							return {
+								kind: "taskItem",
+								value: item,
+							} as TaskItemBlock;
 						}
-
-						if (itemDeadline !== undefined) {
-							if (isSameDay(itemDeadline, folderBlock.value)) {
-								return {
-									kind: "deadline",
-									value: {
-										_id: item._id,
-										title: item.title,
-										isCompleted: item.isCompleted,
-										date: toDate(itemDeadline),
-									},
-								} as DeadlineBlock
-							}
-						}
-
-						return null;
 					}
-				)
-				.filter(i => i !== null);
+
+					if (itemDeadline !== undefined) {
+						if (isSameDay(itemDeadline, folderBlock.value)) {
+							return {
+								kind: "deadline",
+								value: {
+									_id: item._id,
+									title: item.title,
+									isCompleted: item.isCompleted,
+									date: toDate(itemDeadline),
+								},
+							} as DeadlineBlock;
+						}
+					}
+
+					return null;
+				})
+				.filter((i) => i !== null);
 		}
 
 		return [...folderBlocks];
+	}, [taskItems]);
+
+	const extraBlock = useMemo(() => {
+		if (
+			taskItems.some(
+				(i) =>
+					(i.startDate !== undefined &&
+						differenceInCalendarDays(toDate(i.startDate), today()) >= 7) ||
+					(i.deadline !== undefined &&
+						differenceInCalendarDays(toDate(i.deadline), today()) >= 7),
+			)
+		) {
+			const children: (TaskItemBlock | DeadlineBlock)[] = [];
+			for (const item of taskItems) {
+				if (item.startDate === undefined && item.deadline === undefined) continue;
+				if (
+					item.startDate !== undefined &&
+					differenceInCalendarDays(toDate(item.startDate), today()) >= 7
+				) {
+					children.push({
+						kind: "taskItem",
+						value: item,
+					} as TaskItemBlock);
+				}
+
+				if (
+					item.deadline !== undefined &&
+					differenceInCalendarDays(toDate(item.deadline), today()) >= 7
+				) {
+					children.push({
+						kind: "deadline",
+						value: {
+							_id: item._id,
+							title: item.title,
+							isCompleted: item.isCompleted,
+							date: toDate(item.deadline),
+						},
+					} as DeadlineBlock);
+				}
+			}
+
+			return {
+				kind: "date",
+				value: addDays(today(), 7), // Placeholder value, not used for display
+				children,
+			} as DateBlock;
+		}
 	}, [taskItems]);
 
 	return (
@@ -109,6 +158,7 @@ export default function UpcomingLayout({
 				<div key={block.value.toISOString()}>
 					<div className="group flex flex-row items-center gap-4 px-3 select-none">
 						<div className="flex flex-row gap-3 items-end">
+							{/*DAY NUMBER*/}
 							<div className="flex flex-col items-center">
 								{today().getMonth() !== block.value.getMonth() ?
 									<p className="text-sm">
@@ -120,13 +170,18 @@ export default function UpcomingLayout({
 								</h2>
 							</div>
 
-							<div className={`flex flex-row gap-2 items-center ${isToday(block.value) ? "text-white" : "text-slate-400"}`}>
+							{/*DAY NAME*/}
+							<div
+								className={`flex flex-row gap-2 items-center ${isToday(block.value) ? "text-white" : "text-slate-400"}`}
+							>
 								<h2 className="text-xl">
 									{isToday(block.value) ? "Today" : format(block.value, "EEEE")}
 								</h2>
 								<button
 									className="opacity-0 group-hover:opacity-100 text-slate-400 hover:text-white duration-100"
-									onClick={() => taskItemActions.create(undefined, toExactDate(block.value))}
+									onClick={() =>
+										taskItemActions.create(undefined, toExactDate(block.value))
+									}
 								>
 									<IconPlus size={24} />
 								</button>
@@ -136,42 +191,90 @@ export default function UpcomingLayout({
 
 					<hr className="mt-2 mb-4 border-slate-700" />
 
-					{block.children.sort((a, b) => a.kind === "deadline" ? -1 : 1).map((item) => {
-						if (item.kind === "taskItem") {
-							return (
-								<TaskItemLayout
-									key={item.value._id}
-									taskItem={item.value}
-									isSelected={selectedTaskItem === item.value._id}
-									showTodayIcon={true}
-									thisActions={{
-										create: null,
-										modify: taskItemActions.modify.bind(null, item.value._id),
-										delete: null,
-									}}
-									selectThis={() => setSelectedTaskItem(item.value._id)}
-								/>
-							)
-						} else if (item.kind === "deadline") {
-							return (
-								<div
-									className={`group px-4 mb-2 flex flex-row items-center gap-5 ${item.value.isCompleted ? "line-through text-slate-400" : "text-white"}     rounded-lg cursor-pointer duration-200`}
-									onClick={(e) => {
-										e.stopPropagation();
-										setSelectedTaskItem(item.value._id);
-									}}
-									key={item.value._id + "-deadline"}
-								>
-									<RichIcon iconData={Constants.Icons.DEADLINE} size={16} />
-									<p className="text-sm underline-offset-2">
-										<span className="group-hover:underline">{item.value.title}</span> <span className="pl-4 text-slate-400">due at {format(item.value.date, "HH:mm")}</span>
-									</p>
-								</div>
-							)
-						}
-					})}
+					<BlockChildren
+						block={block}
+						selectedTaskItem={selectedTaskItem}
+						setSelectedTaskItem={setSelectedTaskItem}
+						taskItemActions={taskItemActions}
+					/>
 				</div>
 			))}
+			{
+				extraBlock && (
+					<div>
+						<div className="group flex flex-row items-center gap-4 px-3 select-none">
+							<h2 className="text-xl text-slate-400">Following week</h2>
+						</div>
+
+						<hr className="mt-2 mb-4 border-slate-700" />
+
+						<BlockChildren
+							block={extraBlock}
+							selectedTaskItem={selectedTaskItem}
+							setSelectedTaskItem={setSelectedTaskItem}
+							taskItemActions={taskItemActions}
+							specificDeadline
+						/>
+					</div>
+				)
+			}
 		</div>
+	);
+}
+
+export function BlockChildren({
+	block,
+	selectedTaskItem,
+	setSelectedTaskItem,
+	taskItemActions,
+	specificDeadline,
+}: Pick<UpcomingLayoutProps, "selectedTaskItem" | "setSelectedTaskItem" | "taskItemActions"> & {
+	block: DateBlock;
+	specificDeadline?: boolean;
+}) {
+	return (
+		<>
+			{block.children
+				.sort((a, b) => (a.kind === "deadline" ? -1 : 1))
+				.map((item) => {
+					if (item.kind === "taskItem") {
+						return (
+							<TaskItemLayout
+								key={item.value._id}
+								taskItem={item.value}
+								isSelected={selectedTaskItem === item.value._id}
+								showTodayIcon={true}
+								thisActions={{
+									create: null,
+									modify: taskItemActions.modify.bind(null, item.value._id),
+									delete: null,
+								}}
+								selectThis={() => setSelectedTaskItem(item.value._id)}
+							/>
+						);
+					} else if (item.kind === "deadline") {
+						return (
+							<div
+								className={`group px-4 mb-2 flex flex-row items-center gap-5 ${item.value.isCompleted ? "line-through text-slate-400" : "text-white"}     rounded-lg cursor-pointer duration-200`}
+								onClick={(e) => {
+									e.stopPropagation();
+									setSelectedTaskItem(item.value._id);
+								}}
+								key={item.value._id + "-deadline"}
+							>
+								<RichIcon iconData={Constants.Icons.DEADLINE} size={16} />
+								<p className="text-sm underline-offset-2">
+									<span className="group-hover:underline">
+										{item.value.title}
+									</span>{" "}
+									<span className="pl-4 text-slate-400">
+										{specificDeadline ? format(item.value.date, "MM/dd 'at' HH:mm") : format(item.value.date, "HH:mm")}
+									</span>
+								</p>
+							</div>
+						);
+					}
+				})}
+		</>
 	);
 }
